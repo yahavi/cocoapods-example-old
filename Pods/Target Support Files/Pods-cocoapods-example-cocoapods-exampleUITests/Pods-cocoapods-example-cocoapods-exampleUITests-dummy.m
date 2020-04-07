01878fe977898c90968b5ec004cca14ba36b510f
yahavi <yahavi@jfrog.com>
2020-04-07 17:21:07 +0300

    Remove Pods directory

--- Pods/Target Support Files/Pods-cocoapods-example-cocoapods-exampleUITests/Pods-cocoapods-example-cocoapods-exampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_cocoapods_example_cocoapods_exampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_cocoapods_example_cocoapods_exampleUITests
-@end
